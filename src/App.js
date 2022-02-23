f8fdf6ad4213a92c238924c3d043b44ce4154d66
unknown <wallaceomatthew@gmail.com>
2022-02-23 10:36:22 -0600

    search fliter 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 import {Users} from "./user"
 import {useState} from "react";
+import { Table } from './Table';
 
 function App() {
 
   const [query, setQuery] = useState("");
+
+  const keys = ["firstname", "lastname", "email"]
+
+  
+  const Search = (data) => {
+    return data.filter((item) =>
+        keys.some(key=>item[key].toLowerCase().includes(query))
+      );
+  };
 
 
   // console.log(Users.filter(user => user.first_name.toLowerCase().includes("ma")));
@@ -12,13 +22,7 @@ function App() {
   return (
     <div className="App">
       <input type="text" placeholder='Search...' className='search' onChange={e => setQuery(e.target.value)}/>
-      <ul className='list'>
-        {Users.map((user) => (
-           
-           <li key={user.id} className='listItem'>{user.first_name}</li>
-
-        ))}
-      </ul>
+        <Table data={Search(Users)}/>
     </div>
   );
 }
